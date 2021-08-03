b3842b270fcb1efb874dbdc333e9d94b69d67e07
Richard Snider <richardnsnider@gmail.com>
2021-08-03 20:22:10 +0000

    update athena logs table creation query

--- athena_table.sql
+++ athena_table.sql
@@ -1,9 +1,10 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS default.go_server (
+CREATE EXTERNAL TABLE IF NOT EXISTS default.logs (
   `container_id` string,
   `time` timestamp,
   `container_name` string,
-  `log` string,
   `source_code` string,
   `message` string,
   `source` string
-) ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe' WITH SERDEPROPERTIES ('serialization.format' = '1') LOCATION 's3://vorprog-logs/go-server/' TBLPROPERTIES ('has_encrypted_data' = 'false')
+) ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES ('serialization.format' = '1')
+LOCATION 's3://vorprog-logs/logs/' TBLPROPERTIES ('has_encrypted_data' = 'false')
