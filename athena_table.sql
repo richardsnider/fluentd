65a9d26a4aeff65046f56cc22bf3d5f6b39e376a
Richard Snider <richardnsnider@gmail.com>
2021-08-04 03:14:12 +0000

    alter athena table

--- athena_table.sql
+++ athena_table.sql
@@ -1,10 +1,13 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS default.logs (
-  `container_id` string,
   `time` timestamp,
-  `container_name` string,
-  `source_code` string,
   `message` string,
-  `source` string
-) ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-WITH SERDEPROPERTIES ('serialization.format' = '1')
-LOCATION 's3://vorprog-logs/logs/' TBLPROPERTIES ('has_encrypted_data' = 'false')
+  `source_code` string,
+  `source` string,
+  `container_id` string,
+  `container_name` string 
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = '1'
+) LOCATION 's3://vorprog-logs/logs/'
+TBLPROPERTIES ('has_encrypted_data'='false');
